0f08fb62f7edde37dbca913877c806cf28d1f9ba
lacedfent <sillylittlepal@gmail.com>
2026-08-18 06:47:56 -0400

    fix account manager crash from superclass shadow fields

--- src/main/java/keystrokesmod/mixin/impl/accessor/IAccessorGuiScreen.java
+++ src/main/java/keystrokesmod/mixin/impl/accessor/IAccessorGuiScreen.java
@@ -1,14 +1,21 @@
 package keystrokesmod.mixin.impl.accessor;
 
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.util.List;
 
 @SideOnly(Side.CLIENT)
 @Mixin(GuiScreen.class)
 public interface IAccessorGuiScreen {
+    @Accessor("buttonList")
+    List<GuiButton> getButtonList();
+
     @Invoker("mouseClicked")
     void callMouseClicked(int mouseX, int mouseY, int mouseButton);
 }
